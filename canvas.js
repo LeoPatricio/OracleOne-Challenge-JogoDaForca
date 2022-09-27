c7db08c507e77b9e1d9525f6fe2bf46c9809a817
Leonardo Patricio <leo.ps9@hotmail.com>
2022-09-27 20:12:28 -0300

    Finalizada desenho da forca e funções de exibir letras certas e erradas

--- canvas.js
+++ canvas.js
@@ -5,13 +5,25 @@ function drawGame(){
     gameArea.fillStyle = "#E9ECF8";
     gameArea.strokeStyle = "#0A3871";
 
+
+
     gameArea.fillRect(0, 0, 1200, 800);
     gameArea.beginPath();
     gameArea.moveTo(900, 500);
     gameArea.lineTo(650, 500);
+
+    gameArea.moveTo(700,500)
+    gameArea.lineTo(700,100)
+
+    gameArea.moveTo(850,100)
+    gameArea.lineTo(700,100)
+
     gameArea.stroke();
     gameArea.closePath();
+
 }
+
+
 
 function drawLines(){
     gameArea.lineWidth = 8;
@@ -28,3 +40,83 @@ function drawLines(){
     gameArea.stroke();
     gameArea.closePath();
 }
+
+function writeCorrectLetter(index){
+    gameArea.font = "bold 52px Ariel";
+    gameArea.linaCap = "round";
+    gameArea.fillStyle = "#0A3871";
+    gameArea.lineWidth = 6;
+    let widthLetter = 600 / wordSelected.length;
+    gameArea.fillText(wordSelected[index], 505 + (widthLetter * index), 620);
+
+}
+
+function writeErroLetter(letter, error){
+    gameArea.font = "bold 40px Ariel";
+    gameArea.linaCap = "round";
+    gameArea.fillStyle = "#0A3871";
+    gameArea.lineWidth = 6;
+    gameArea.fillText(letter, 920 + (-50 * error), 720, 40)
+}
+
+function drawDoll(){
+    gameArea.lineWidth = 8;
+    gameArea.lineCap = "round";
+    gameArea.lineJoin = "round";
+    gameArea.strokeStyle = "#0A3871";
+
+    if(error === 6){
+    gameArea.moveTo(850,100);
+    gameArea.lineTo(850,171);
+    }
+    if(error === 5){
+    gameArea.moveTo(900, 230);
+    gameArea.arc(850, 230, 50, 0, Math.PI * 2);
+    }
+    if(error === 4){
+    gameArea.moveTo(850,389);
+    gameArea.lineTo(850,289);
+    }
+    if(error === 3){
+    gameArea.moveTo(850,389);
+    gameArea.lineTo(800,450);
+    }
+    if(error === 2){
+    gameArea.moveTo(850,389);
+    gameArea.lineTo(890,450);
+    }
+    if(error === 1){
+    gameArea.moveTo(850,330);
+    gameArea.lineTo(800,389);
+    }
+    if(error === 0){
+    gameArea.moveTo(850, 330);
+    gameArea.lineTo(890, 389);
+    }
+    gameArea.stroke()
+    gameArea.closePath()
+
+}
+function showWin(){
+    gameArea.font = "bold 42px Ariel";
+    gameArea.lineWidth = 6;
+    gameArea.lineCap = "round"
+    gameArea.lineJoin = "round"
+    gameArea.fillStyle = "blue"
+    gameArea.fillText("Fim de jogo, você VENCEU!", 520, 80);
+}
+
+function showDefeat(){
+    gameArea.font = "bold 42px Ariel";
+    gameArea.lineWidth = 6;
+    gameArea.lineCap = "round"
+    gameArea.lineJoin = "round"
+    gameArea.fillStyle = "red"
+    gameArea.fillText("Fim de jogo, você PERDEU!", 520, 80);
+  }
+  
+
+function reload(){
+    location.reload();
+}
+
--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let gameArea = document.querySelector(".forca").getContext("2d");
 let words = ["PROGRAMACAO", "CODAR", "ALURA", "ONE", "ORACLE", "JAVASCRIPT", "PALAVRA", "FORCA", "CHALLENGE", "JOGO"];
 let wordSelected  = "";
 
+let letters = [];
+let error = 6;
+
 //Functions
 
 function randomWord(){
@@ -14,12 +17,54 @@ function randomWord(){
   console.log(wordSelected);
 }
 
+function checkLetter(key){
+  let status = false;
+  if(key >= 65 && letter.indexOf(key) || key <= 90 && letter.indexOf(key)){
+    letters.push(key);
+    console.log(key);
+    console.log(letters);
+    return status;
+  }else{
+    status = true;
+    letters.push(key);
+    console.log(key);
+    console.log(letters);
+    return status;
+  }
+}
+
+function erroCounter(letter){
+  error -= 1;
+  drawDoll();
+}
+
 function start(){
   randomWord();
   drawGame();
   drawLines();
   switchDiv();
+  drawDoll();
 
+  document.onkeydown = (e) => {
+    let letter = e.key.toUpperCase();
+    if(checkLetter(letter) && wordSelected.includes(letter)){
+      for(let i = 0; i < wordSelected.length; i++){
+        if(wordSelected[i] === letter){
+          writeCorrectLetter(i);
+        }
+      }
+    }else{
+      erroCounter(letter);
+      writeErroLetter(letter, error);
+
+    if(error <= 0){
+        showDefeat();
+        setTimeout(reload, 1000);
+    }
+    
+  }
+ 
+  }
 }
 
 function addNewWords(){
@@ -30,3 +75,4 @@ function switchDiv(){
     secBtn.style.display = "none";
     game.style.display = "block";
 }
+
